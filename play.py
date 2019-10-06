b5ede4ef8781de42189701be35c9a1e384909899
Gurvan <gurvanpriem@gmail.com>
2019-10-06 10:47:55 +0200

    Added missing play.py

--- play.py
+++ play.py
@@ -33,4 +33,3 @@ with torch.no_grad():
         obs, reward, done, infos = env.step(actions)
         if done:
             break
-        # print("Player 1 action state: ", env.obs.players[0].action_state, "\t\tPlayer 2 actions_state:", env.obs.players[1].action_state)
